3ed73966502d060c52635a18735d7ec6a84ea5a2
samuel-ponraj <samuelponraj.official@gmail.com>
2026-01-26 21:31:26 +0530

    updated

--- components/home/HomePage.js
+++ components/home/HomePage.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 "use client";
 
 import styles from "./HomePage.module.css";
@@ -69,48 +68,3 @@ const HomePage = () => {
 };
 
 export default HomePage;
-=======
-
-import styles from './HomePage.module.css'
-import { ArrowRight, CheckCircle } from 'lucide-react';
-import Features from './Features'
-import Link from 'next/link';
-import UseCase from './UseCase'
-import HowItWorks from './HowItWorks'
-
-const HomePage = () => {
-  return (
-    <div className={styles.homePagecontainer}>
-        <section className={styles.heroSection}>
-          <p>✨ Simple & Efficient Attendance Management</p>
-          <h1>Track Attendance with <span style={{color:'var(--primary)'}}>Ease & Precision</span></h1>
-          <p>Streamline your team's attendance tracking. Add teams, manage members, and record attendance in just a few clicks.</p>
-          <div className={styles.heroButtons}>
-            <Link href='/dashboard'>
-              <button className={styles.homeBtn1}>Record Attendance <ArrowRight /></button>
-            </Link>
-            <Link href='#features'>
-            <button className={styles.homeBtn2}>Learn More</button>
-            </Link>
-          </div>
-          <div className={styles.highlights}>
-            <div className={styles.highlight}>
-              <CheckCircle style={{color:'var(--primary)'}}/>
-              <span>Real-time tracking</span>
-            </div>
-            <div className={styles.highlight}>
-              <CheckCircle style={{color:'var(--primary)'}} />
-              <span>Team management</span>
-            </div>
-            <div className={styles.highlight}>
-              <CheckCircle style={{color:'var(--primary)'}} />
-              <span>Easy to use</span>
-            </div>
-          </div>
-        </section>
-    </div>
-  )
-}
-
-export default HomePage
->>>>>>> c39aa9d3570ced9499a5f3473f6b937ca0c693a8
