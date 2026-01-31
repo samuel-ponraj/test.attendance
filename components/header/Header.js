862f207ef31dc6d82c172cee689212184d92f145
samuel-ponraj <samuelponraj.official@gmail.com>
2026-01-31 17:13:34 +0530

    updated

--- components/header/Header.js
+++ components/header/Header.js
@@ -5,6 +5,7 @@ import { LogOut } from 'lucide-react'
 import { useAuth } from '../../app/context/AuthContext'
 import { useRouter } from 'next/navigation'
 import styles from './Header.module.css'
+import Link from 'next/link'
 
 const Header = () => {
   const [isScrolled, setIsScrolled] = useState(false)
@@ -29,7 +30,9 @@ const Header = () => {
   return (
     <div className={`${styles.headerContainer} ${isScrolled ? styles.scrolled : ''}`}>
       <div className={styles.header}>
+        <Link href='/'>
         <Image src='/logo/KDA-logo-white.png' alt='KDS Logo' width={150} height={100}/>
+        </Link>
         <div style={{display:'flex', alignItems:'center', gap:'30px'}}>
           <button onClick={handleAuthClick}>
             {user ? (
